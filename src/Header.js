21f801c658e87d4b9328875fe28165ec576ee2a0
Aggie Koko <agata.kosior57@gmail.com>
2018-07-30 13:55:39 +0200

    header content+style

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="header">
+            <h1>wrocfavs</h1>
+            </div>
+        )
+    }
+}
+export default Header
