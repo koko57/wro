9ef75010e49b14ec4e873b5ab8c3bd7ceddb89b2
Aggie Koko <agata.kosior57@gmail.com>
2018-07-30 14:05:54 +0200

    places moved from Map to App state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './Map'
+import Sidebar from './Sidebar';
+import Header from './Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      places: [{
+        fullName: 'Bema Cafe',
+        lat: 51.1182005,
+        lng: 17.0400579,
+        type: 'break'
+      }, {
+        fullName: 'Gniazdo',
+        lat: 51.105362,
+        lng: 17.0315874,
+        type: 'coffee'
+      }, {
+        fullName: 'Paloma',
+        lat: 51.1094935,
+        lng: 17.028659,
+        type: 'coffee'
+      }, {
+        fullName: 'Etno Okrąglak',
+        lat: 51.10238529999999,
+        lng: 17.0300293,
+        type: 'coffee'
+      }]
+    }
+  }
+
   render() {
     return (
-      <Map />
+      <div className="app">
+        <Header />
+        <Sidebar places={this.state.places} />
+        <Map places={this.state.places}/>
+      </div>
     );
   }
 }
--- src/Map.js
+++ src/Map.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 class Map extends Component {
-   render() {
-   const GoogleMapExample = withScriptjs(withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 51.110903, lng: 17.035538 } }
-        defaultZoom = { 15 }>
-        <Marker
-      position={{ lat: 51.1182005, lng: 17.0400579 }}
-    />
-    <Marker
-      position={{ lat: 51.105362,
-        lng: 17.0315874 }}
-    />
-    <Marker
-      position={{ lat: 51.1094935,
-        lng: 17.028659 }}
-    /><Marker
-    position={{ lat: 51.10238529999999,
-        lng: 17.0300293 }}
-  />
     
-    
-      </GoogleMap>
-     
+    render() {
+        let coffeeSpots = this.props.places.map(loc => 
+            (<Marker position={{lat: loc.lat, lng: loc.lng }} key={loc.fullName}>
+              <InfoWindow><p>{loc.fullName}</p></InfoWindow>
+            </Marker>)
+        ); 
+        const GoogleMapExample = withScriptjs(withGoogleMap(props => (
+            <GoogleMap
+                defaultCenter = {{ lat: 51.110903, lng: 17.033538 }}
+                defaultZoom = { 14 }>
+                {coffeeSpots}
+            </GoogleMap>
    )));
    return(
       <div>
         <GoogleMapExample
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyB05dcc0ITwKkEM0PomMgtsobDCE2evMHM&libraries=drawing,places"
         loadingElement={<div style={{ height: '100%' }} />}
-          containerElement={ <div style={{ height: '100vh', width: '80%' }} /> }
-          mapElement={ <div style={{ height: '100%' }} /> }
+        containerElement={ <div style={{ height: 'calc(100vh - 70px)', width: '80%', float: 'right' }} /> }
+        mapElement={ <div style={{ height: '100%' }} /> }
         />
       </div>
    );
