3cfde3f7a56ddf3c128aa4c400caecb13564dec2
Aggie Koko <agata.kosior57@gmail.com>
2018-08-08 23:39:55 +0200

    header moved components folder, favicon added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="header">
+                <h1>wrocfavs</h1>
+            </div>
+        )
+    }
+}
+export default Header
