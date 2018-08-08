3b41219fd442d3b340ab4b92ded01341e10bc907
Aggie Koko <agata.kosior57@gmail.com>
2018-08-09 00:12:05 +0200

    sidebar styling and finishing touches in js file

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import './Sidebar.scss'
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faBars } from '@fortawesome/free-solid-svg-icons';
-
-library.add([faChevronDown, faBars]);
 
 const Sidebar = (props) => {
-  
+
   const select = (e) => {
     props.filterPlaces(e.target.value);
   }
   const search = (e) => {
-    props.filterByName(e.target.value)
+    props.filterByName(e.target.value);
   }
-        return (
-          <div className="sidebar">
-          <input type="text" name="searchByName" id="search" placeholder="Search By Name" onChange={search}/>
-            <select name="placeFilter" id="placeFilter" onChange={select}>
-              <option value="none" disabled>Filter</option>
-              <option value="coffee">Coffee</option>
-              <option value="techno">Techno</option>
-              <option value="art">Art</option>
-              <option value="" >All</option>
-            </select>
-            <FontAwesomeIcon icon="chevron-down"/>
-            <ul className="places-list" id="places-list">
-              {props.places.map(place => <li className="places-list--item" onClick={props.selectMarker} id={place.name} key={place.name}>{place.name}</li>)}
-            </ul>
-          </div>
-        )
-    
+  return (
+    <div className="sidebar">
+      <div className="search-filter-bar">
+        <input type="text" name="searchByName" id="search" placeholder="Search By Name" onChange={search}/>
+        <select name="placeFilter" id="placeFilter" onChange={select}>
+          <option value="" >All</option>
+          <option value="coffee">Coffee</option>
+          <option value="techno">Techno</option>
+          <option value="art">Art</option>
+        </select>
+      </div>
+      <div className="places-list-container">
+        <ul className="places-list" id="places-list">
+          {props.places.map(place => 
+          <li className="places-list--item" onClick={props.selectMarker} id={place.name} key={place.name}>
+          {/* clicking the button opens the infoWindow of linked marker */}
+            <button onClick={props.selectMarker} id={place.name}>{place.name}</button>
+          </li>)}
+        </ul>
+      </div> 
+    </div>
+  )
 }
 
 export default Sidebar
